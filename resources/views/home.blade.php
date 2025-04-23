6c45807b1aebda65671fb35ed118d5269514407d
noorshallouf <noorshallouf02@gmail.com>
2025-04-23 17:58:02 +0300

    home update

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <img src="pic1.png"  height="100" width="100" alt="" id="t" > 
+    <img src="pic2.jpeg" height="100 " width="100" alt="">
+    <img src="pic3.jpeg" height="100" width="100" alt=""> 
+    <br>
+    <a href="https://www.google.com/" target="iframe2"></a>
+    <iframe src="page.html" name="iframe1" width="350" height="350"></iframe>
+    <iframe src="page2.html" name="iframe2" width="350" height="350"></iframe>
+   <p> </p>
+
+</body>
+</html>
